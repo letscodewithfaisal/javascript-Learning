69d76c3f54174016d53f97ef0e32617a0ca2def2
Faisal Siddiqui <cvraman902@gmail.com>
2026-04-30 12:55:56 +0000

    learn about variable

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 134576
+let accountEmail = "faisal1234@gmail.com"
+var accountPassword = "123456"
+ 
+accountEmail = "hdfsc@gmail.com"
+accountPassword = "12455877"
+accountCity = "Delhi"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
