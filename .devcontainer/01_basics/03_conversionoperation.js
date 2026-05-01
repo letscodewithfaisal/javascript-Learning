4d73c3f1d1726c99f264075ce14210d95bf2f45a
Faisal Siddiqui <cvraman902@gmail.com>
2026-05-01 12:12:51 +0000

    jsconversionop

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,15 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// '33' is converting into number then easily converted into number(short me =>)
+
+// '33abc' => NaN
+// true => 1; false=>0
+
