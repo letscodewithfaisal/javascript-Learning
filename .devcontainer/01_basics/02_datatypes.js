2ac9b6df63a4032707556c3cc7554216fddca69d
Faisal Siddiqui <cvraman902@gmail.com>
2026-05-01 09:47:54 +0000

    updated commit

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -2,3 +2,22 @@
 
 // alert(3 + 3) // we are using nodejs, not browser
 
+console.log("Faisal")
+
+let name = 'faisal'
+let age = 24
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+//bigint 
+// string => ""//'' use single or double quote
+// boolean => true/false
+// null => standalone value
+//  undefine =>
+    // symbol => unique
+
+// object 
+
+console.log(typeof undefined); // rsult undefine hi ayega
+console.log(typeof null); // object ayega result
