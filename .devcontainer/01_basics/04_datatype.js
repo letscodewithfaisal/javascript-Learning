6974b6036c20cf907fe1afc22de0cb738556794b
Faisal Siddiqui <cvraman902@gmail.com>
2026-05-06 14:54:26 +0000

    05_datatypes

---
+++ .devcontainer/01_basics/04_datatype.js
@@ -0,0 +1,12 @@
+
+// two types of data type
+// 1. #premitive
+// 7 types: strng, number, boolean, null, undefined, symbol,BigInt
+
+
+
+
+// 2. #Refrence(Non-Premitive)
+
+// Array, Objects, Functions
+
