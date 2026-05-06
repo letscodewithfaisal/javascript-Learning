36c8dd2e275f216b7ad31a508a08c46469d88f96
Faisal Siddiqui <cvraman902@gmail.com>
2026-05-06 18:28:09 +0000

    first

---
+++ .devcontainer/01_basics/stack_vs_Heap.js
@@ -0,0 +1,22 @@
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack -> (premitive), Heap-> (Non-Premtive)
+let myYoutubename = "exploringworld"
+
+let anotherName = myYoutubename
+anotherName = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@google.com",
+    city: "NewDelhi"
+}
+
+let userTwo = userOne
+
+userTwo.email = "faisal@google.com"
+
+console.log(userOne.email);
+console.log(userTow.email);
