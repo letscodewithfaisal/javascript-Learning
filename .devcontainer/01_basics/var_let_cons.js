3e1a1b0e57df2324f91dd97ae8e997fc0e8f58b6
Faisal Siddiqui <cvraman902@gmail.com>
2026-05-13 18:30:22 +0000

    one change

--- .devcontainer/01_basics/var_let_cons.js
+++ .devcontainer/01_basics/var_let_cons.js
@@ -1,8 +1,20 @@
 // console.log("testing")
  
-var age = 23
-var num1 = 10;
-var num2 = 20;
+// var age = 23
+// var num1 = 10;
+// var num2 = 20;
 
 
-console.log(num1 + num2);
+// console.log(num1 + num2);
+
+var age = 22;
+var firstName = "Faisal"
+
+//Naming Conv.
+// firstname
+// first_name
+// FirstName Clasess
+// firstName (pref) Cammel Casing
+
+console.log(age)
+console.log(firstName)
