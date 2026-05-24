ae0a86a2d446f86439328faf815c5599c813a165
letscodewithfaisal <cvraman902@gmail.com>
2026-05-24 09:35:45 +0530

    operator assign

---
+++ condition_statement.js
@@ -0,0 +1,32 @@
+// if-else 
+// const age = 42;
+
+// if (age>=18) {
+//     console.log('yes, you can vote')
+// }
+// else{
+//     console.log('No, you can not vote')
+// }
+
+//else if ad elseif
+// const age = 40;
+
+// if (age<=20) {
+//     console.log('yes, you can vote')
+// }
+// else if(age>=32){
+//     console.log('you are an adult')
+
+// }
+// else{
+//     console.log('No, you can not vote')
+// }
+
+// Ternary Case for only two case 
+const age = 17;
+// age >= 18 ? console.log('yes') : console.log('No')
+
+// let result = age >= 18 ? 'yes' : 'No'
+// console.log(result)
+
+
