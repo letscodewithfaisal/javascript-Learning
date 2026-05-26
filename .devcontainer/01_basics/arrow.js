bb63d72990f1f7b582b92f86e6518e52a695c38b
Faisal Siddiqui <cvraman902@gmail.com>
2026-05-26 18:11:42 +0000

    arrow explain

---
+++ .devcontainer/01_basics/arrow.js
@@ -0,0 +1,19 @@
+// function greet() {
+//   console.log("Hello");
+// }
+
+// greet();
+
+// 2nd example
+
+// const greet = () => {
+//   console.log("Hello");
+// };
+
+// greet();
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// console.log(add(2, 3));
