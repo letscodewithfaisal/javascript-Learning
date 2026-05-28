5948a15c43c360898db012070167305744f9696a
Faisal Siddiqui <cvraman902@gmail.com>
2026-05-28 18:05:47 +0000

    new commit

--- .devcontainer/01_basics/arrow2.js
+++ .devcontainer/01_basics/arrow2.js
@@ -9,8 +9,19 @@
   }); */}
 
   // 2. Using map() in Arrays
-  const prices = [100, 200, 300];
+//   const prices = [100, 200, 300];
 
-const updatedPrices = prices.map(price => price + 18);
+// const updatedPrices = prices.map(price => price + 18);
 
-console.log(updatedPrices);
+// console.log(updatedPrices);
+
+// 3. Filtering Users
+const users = [
+  { name: "Tony", active: true },
+  { name: "Rahul", active: false },
+  { name: "Aman", active: true }
+];
+
+const activeUsers = users.filter(user => user.active);
+
+console.log(activeUsers);
