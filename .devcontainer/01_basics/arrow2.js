f8e89209508118c0efe5473b4f27d2cc580e48a5
Faisal Siddiqui <cvraman902@gmail.com>
2026-05-28 18:00:41 +0000

    arrow learning

---
+++ .devcontainer/01_basics/arrow2.js
@@ -0,0 +1,16 @@
+// Button Click Event
+{/* <button id="btn">Click Me</button>
+
+
+  const button = document.getElementById("btn");
+
+  button.addEventListener("click", () => {
+    alert("Button Clicked!");
+  }); */}
+
+  // 2. Using map() in Arrays
+  const prices = [100, 200, 300];
+
+const updatedPrices = prices.map(price => price + 18);
+
+console.log(updatedPrices);
